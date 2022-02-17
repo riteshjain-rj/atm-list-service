254137ca3e3e8b8c6cbba5cd92fb5c15a5998b46
Ritesh Jain <riteshjain@gmail.com>
2022-02-17 13:49:46 +0000

    ATMList: Initial code commit changes for Open API swagger

--- src/main/java/com/atm/list/ATMListApplication.java
+++ src/main/java/com/atm/list/ATMListApplication.java
@@ -1,11 +1,14 @@
 package com.atm.list;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ATM List API", version = "1.0", description = "This exercise uses the open-api for ATMs Information"))
 public class ATMListApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/atm/list/controller/ATMListController.java
+++ src/main/java/com/atm/list/controller/ATMListController.java
@@ -2,6 +2,12 @@ package com.atm.list.controller;
 
 import com.atm.list.model.ATMListResponse;
 import com.atm.list.service.ATMListService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,13 +20,23 @@ import java.util.List;
 @RequestMapping(value = "/api")
 public class ATMListController {
 
-
     @Autowired
     @Qualifier("atmListService")
     ATMListService atmListService;
 
+    @Operation(summary = "Get ATM list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the ATM list",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ATMListResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid  identification id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "ATM list not found",
+                    content = @Content) })
     @GetMapping("/atms")
-    public List<ATMListResponse> getATMList(@RequestHeader(name = "identification") long identification) {
+    public List<ATMListResponse> getATMList(
+            @Parameter(description = "identification id for of ATM list to be searched")
+            @RequestHeader(name = "identification") long identification) {
         log.info("Controller: Fetching ATM list with id {}", identification);
         return atmListService.getATMList(identification);
     }
